d5120218965a17688c5a8e1e9ce7e77e80388125
Dave Goehrig <dave@dloh.org>
2017-12-28 21:47:04 -0500

    basically working

--- lib/pgproc.rb
+++ lib/pgproc.rb
@@ -1,17 +1,22 @@
 require "pgproc/version"
 require "pg"
+require 'pp'
 
-module Pgproc
-	def initialize(schema)
-		pg = PG.connect( dbname: 'customers' )
-		pg.exec("select proc.proname::text from pg_proc proc join pg_namespace namesp on proc.pronamespace = namesp.oid where namesp.nspname = '" + schema + "'") do |name|
-			define_method name do |*args|
+module Pg
+	class Proc
+	def initialize(database,schema,user,password)
+		@pg = PG.connect( dbname: database,user: user, password: password)
+		@pg.exec("select proc.proname::text from pg_proc proc join pg_namespace namesp on proc.pronamespace = namesp.oid where namesp.nspname = '" + schema + "'").each do |result|
+			pp result
+			name = result["proname"]
+			define_singleton_method name do |*args|
 				query = "select " + name + "("
 				vars = []	
-				(1..args.length).each do |i| vars.push('$' + i) end
+				(1..args.length).each do |i| vars.push('$' + i.to_s) end
 				query += vars.join(',') + ")"
-				pg.exec(query,args)
+				@pg.exec(query,args).getvalue(0,0)
 			end
 		end
+	end
 	end
 end
